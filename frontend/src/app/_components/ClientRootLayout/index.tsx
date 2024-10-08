ae1b9f64b50961a96f7b18960eb4c4024539d403
sasaiatocos <sasai@atocos.com>
2024-10-08 13:20:59 +0900

    認証の修正

--- frontend/src/app/_components/ClientRootLayout/index.tsx
+++ frontend/src/app/_components/ClientRootLayout/index.tsx
@@ -2,10 +2,13 @@
 
 import type { ReactNode } from 'react';
 import React from 'react';
+import { SessionProvider } from 'next-auth/react';
 import * as Layout from '@/app/ui/Layout';
 
 export function ClientRootLayout({ children }: { children: ReactNode }) {
   return (
-    <Layout.Root>{children}</Layout.Root>
+    <SessionProvider>
+      <Layout.Root>{children}</Layout.Root>
+    </SessionProvider>
   );
 }
--- frontend/src/app/_components/LayoutHeader/index.tsx
+++ frontend/src/app/_components/LayoutHeader/index.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import * as Layout from "@/app/ui/Layout";
+import { useSession } from 'next-auth/react';
+import * as Layout from '@/app/ui/Layout';
 
 type Props = {
   showDrawerMenu?: boolean;
 };
 
 export function LayoutHeader({ showDrawerMenu }: Props) {
+  const { data: session } = useSession();
   return (
     <Layout.Header
       isLogin={Boolean(session)}
